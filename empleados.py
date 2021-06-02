69d0bd91a7972846ff635cd7afb67e6d1b6fd837
Eduardo <eduroblesc@outlook.com>
2021-06-01 20:26:23 -0500

    se agrega menú navegación empleados

--- empleados.py
+++ empleados.py
@@ -1,15 +1,13 @@
 # from recargos import *
 import sqlite3
-
-def menu():
+seleccion = 0
+while seleccion == 0:
     print('Escoge una opcion')
     seleccion = int(input('''
         1. Agregar nuevo empleado, 
         2. consultar empleado,
         3. Salir.
         Ingrese opción: '''))
-    return seleccion
-seleccion = menu()
 print(seleccion)
 
 
@@ -28,41 +26,45 @@ print(seleccion)
 
 
 
-def nuevoEmpleado(id, nombre, cargo, salario, fechaIngreso):
-    datos = [(int(id), nombre, cargo, salario, fechaIngreso)]
-
-    miConexion = sqlite3.connect('Empleados')
-    miCursor = miConexion.cursor()
-    miCursor.executemany("INSERT INTO EMPLEADOS VALUES(?,?,?,?,?)", datos)
-    print ('DATOS ALMACENADOS CON EXITO')
-    miConexion.commit()
-    miConexion.close()
-
-
-def consultarEmpleados(id):
-    # id = id
-    miConexion = sqlite3.connect('Empleados')
-    miCursor = miConexion.cursor()
-    miCursor.execute(f'SELECT * FROM EMPLEADOs WHERE ID={id}')
-    empleados = miCursor.fetchall()
-    for i in empleados:
-        print (i)
-    miConexion.commit()
-    miConexion.close()
-
-
 while seleccion == 1:
     id = input('Inserte número de identificación. Sin puntos ni comas: ')
     nombre = input('Inserte nombres completos del empleado: ')
     cargo = input('Cargo del empleado: ')
     salario = int(input('Ingrese salario del empleado. Sin puntos ni comas: '))
     fIngreso = input('Ingrese fecha de ingreso. dd/mm/aaaa: ')
+    def nuevoEmpleado(id, nombre, cargo, salario, fechaIngreso):
+        datos = [(int(id), nombre, cargo, salario, fechaIngreso)]
+
+        miConexion = sqlite3.connect('Empleados')
+        miCursor = miConexion.cursor()
+        miCursor.executemany("INSERT INTO EMPLEADOS VALUES(?,?,?,?,?)", datos)
+        print ('DATOS ALMACENADOS CON EXITO')
+        miConexion.commit()
+        miConexion.close()
     nuevoEmpleado(id, nombre, cargo, salario, fIngreso)
-    menu()
+    print('Escoge una opcion')
+    seleccion = int(input('''
+        1. Agregar nuevo empleado, 
+        2. consultar empleado,
+        3. Salir.
+        Ingrese opción: '''))
 while seleccion == 2:
     id = int(input('Ingrese ID: '))
-    consultarEmpleados(id)
-    menu()
+    miConexion = sqlite3.connect('Empleados')
+    miCursor = miConexion.cursor()
+    miCursor.execute(f'SELECT * FROM EMPLEADOS WHERE ID={id}')
+    empleados = miCursor.fetchall()
+    for i in empleados:
+        print (i)
+    miConexion.commit()
+    miConexion.close()
+    print('Escoge una opcion')
+    seleccion = int(input('''
+        1. Agregar nuevo empleado, 
+        2. consultar empleado,
+        3. Salir.
+        Ingrese opción: '''))
+
 while seleccion == 3:
     print ('Has salido.')
     seleccion = 0
